70456be41eb54c737d0acd736906554c11561e67
open cv <112242723+Yaswanth111494@users.noreply.github.com>
2023-10-18 09:02:26 +0530

    Add files via upload

---
+++ 2.R
@@ -0,0 +1,12 @@
+# Creating data for the pie chart
+x <- c(20, 65, 15, 50)
+labels <- c("India", "America", "Shri Lanka", "Nepal")
+
+# Giving the chart file a name
+png(file = "Country.jpg")
+
+# Plotting the pie chart
+pie(x, labels)
+
+# Saving the file
+dev.off()
