55b3990cda68a07a43442be22971399546fc99af
open cv <112242723+Yaswanth111494@users.noreply.github.com>
2023-10-18 09:10:00 +0530

    Add files via upload

---
+++ 15.R
@@ -0,0 +1,9 @@
+plot(v,type,col,xlab,ylab) 
+# Creating the data for the chart.  
+v <- c(13,22,28,7,31)  
+# Giving a name to the chart file.  
+png(file = "line_graph.jpg")  
+# Plotting the bar chart.   
+plot(v,type = "o")  
+# Saving the file.  
+dev.off()  
